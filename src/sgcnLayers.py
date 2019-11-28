012345dd69076fbcd254610a6ac41c83b35a2dae
Vivek Reddy Karri <vivekreddykarri98@gmail.com>
2019-11-28 18:29:45 -0500

    First Iteration completed for Defining Layer 1

--- src/sgcnLayers.py
+++ src/sgcnLayers.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 class DS():
-	def __init__(self, WB, WU, hB, hU, zUB, MLG):
+	def __init__(self, WB, WU, hB, hU, zUB, MLG, adj_pos, adj_neg):
 		''' Input Tensors '''
 		self.WB = WB
 		self.WU = WU
@@ -20,11 +20,45 @@ class Layer1(tf.keras.layers.Layer):
 		self.WB0 = WB0
 		self.h0 = h0
 
-	def call(self, inputs, ds, adj_pos, adj_neg, **kwargs):
+	def call(self, d, start, end, adj_pos, adj_neg, **kwargs):
 		''' Inputs are assumed to be Node Id's given as a 1-d list of form [[]]''' 
-		''' kwargs should have two adj_lists, with in and out nodes information '''
-		''' An Adj_List should be of format map of lists '''
+		''' kwargs should have two adj_matrices, with in and out nodes information '''
 		''' ds object having all the required matrices '''
+
+		self_vectors = tf.slice(self.h0, [start, 0], [end-start, tf.shape(self.h0)[-1]]) #shape = b, d_in
+
+		''' For Balanced Sets '''
+		mask_pos_neigh = tf.slice(adj_pos, [start,0], [end-start,tf.shape(adj_pos)[-1]]) # shape = b, N where b = batch_size
+		sum_neigh = tf.reduce_sum(mask_pos_neigh, 0)    # shape = b, 1
+		sum_pos_vectors = tf.matmul(mask_pos_neigh, self.h0, transpose_b=False) # shape = b, d_in
+		sum_pos_vectors = sum_pos_vectors / tf.reshape(sum_neigh, (-1, 1)) # Shape = b, d_in
+		pos_vectors = tf.concat([sum_pos_vectors, self_vectors], 1) #shape = b, 2*d_in
+		d.WB = tf.assign(d.WB[start:end, :, 0], tf.matmul(pos_vectors, self.WB0, transpose_b=True)) #shape = N, d_out, L
+
+		''' For Unbalanced Sets '''
+
+		mask_neg_neigh = tf.slice(adj_neg, [start,0], [end-start,tf.shape(adj_neg)[-1]]) # shape = b, N where b = batch_size
+		sum_neigh = tf.reduce_sum(mask_neg_neigh, 0)    # shape = b, 1
+		sum_neg_vectors = tf.matmul(mask_neg_neigh, self.h0, transpose_b=False) # shape = b, d_in
+		sum_neg_vectors = sum_neg_vectors / tf.reshape(sum_neigh, (-1, 1)) # Shape = b, d_in
+		neg_vectors = tf.concat([sum_neg_vectors, self_vectors], 1) #shape = b, 2*d_in
+		d.WU = tf.assign(d.WU[start:end, :, 0], tf.matmul(neg_vectors, self.WU0, transpose_b=True)) #shape = N, d_out, L
+
+		return d
+
+
+
+
+
+
+
+		
+		
+
+		
+
+
+
     	tf.add(tf.gather_nd(self.h0, inputs))
 
 class lossOptimize():
